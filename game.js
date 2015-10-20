4b13a9ec977c7717fa77e7bbe7e2c6f98e5348dd
GuineaGuy99 <GuineaGuy99.Game@gmail.com>
2015-10-20 23:27:19 +1100

    Classes!

--- game.js
+++ game.js
@@ -2,3 +2,13 @@ GCE.Start({
 	canvas: 'game',
 	frameRate: 1
 })
+
+GCE.CreateBlueprint('Player', {
+	init: function(){
+		this._super();
+		this.AddComponet('Sprite');
+	},
+	update: function(){
+
+	},
+});
