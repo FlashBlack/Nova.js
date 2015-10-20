4590a72384867729f15af3fb1db62a0fb70e70fe
Josh Boomhower <encryptedcow@gmail.com>
2015-10-20 03:33:29 -0600

    main
    began making the awesomness

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('GUINEAENGINE IS BETTER');
+var GCE = new function() {
+	this.VERSION = '0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.1';
+
+
+	// holds all the instances of entities
+	var Entities = {};
+	// holds blueprint for each entity
+	var EntityBlueprints = {};
+
+	var gameLoop = function() {
+
+	}
+
+	
+	this.Start = function(parameters) {
+		
+	}
+}
