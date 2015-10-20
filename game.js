1d8f981affd096191951ca76832c73639f7a4879
Josh Boomhower <encryptedcow@gmail.com>
2015-10-20 04:22:34 -0600

    progress
    we're gettin there!

---
+++ game.js
@@ -0,0 +1,3 @@
+GCE.Start({
+	canvas: 'game'
+})
--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 var GCE = new function() {
 	this.VERSION = '0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.1';
 
-
 	// holds all the instances of entities
 	var Entities = {};
 	// holds blueprint for each entity
@@ -10,13 +9,40 @@ var GCE = new function() {
 	var gameLoop = function() {
 
 	}
-
 	
 	this.Start = function(parameters) {
-		if (!parameters.hasOwnProperty('canvas')) return false; //if no canvas was passed exit early
-		if (!parameters.hasOwnProperty('width')) parameters.width = 800; //if nothing was passed, set to defaut
-		if (!parameters.hasOwnProperty('height')) parameters.height = 600; //if nothing was passed, set to defaut
+		parameters = SetDefaultParameters(parameters);
 
-		
+		// ensure that a canvas id was passed
+		if (!parameters.hasOwnProperty('canvas')) {
+			console.error('Canvas ID must be passed!');
+			return false; //if no canvas was passed exit early
+		} else {
+			var canvas = document.getElementById(parameters.canvas)
+			if(canvas == undefined) {
+				console.error('Invalid Canvas ID!');
+				return false;
+			} else {
+				// finding the canvas was a success
+				this.canvas = canvas;
+				this.ctx = canvas.getContext('2d');
+			}
+		}
+	}
+
+	function SetDefaultParameters(parameters) {
+		var defaultParameters = {};
+
+		for(var p in parameters) {
+			var currentParameter = parameters[p];
+			if(!defaultParameters.hasOwnProperty(p)) {
+				defaultParameters[p] = currentParameter;
+			} 
+			// if the type matches, set the passed value. else leave as default
+			else if(typeof defaultParameters[p] == typeof currentParameter) {
+				defaultParameters[p] = currentParameter;
+			}
+		}
+		return defaultParameters;
 	}
 }
