154ddd683860a3575166ff96e27a04bdd52e4514
Josh Boomhower <encryptedcow@gmail.com>
2015-10-21 00:46:16 -0600

    commented stuff
    lots of comments!

--- main.js
+++ main.js
@@ -1,28 +1,36 @@
 var GCE = new function() {
 	this.VERSION = '0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.1';
-	this.ready = false;
+
+	// becomes true when all assets are loaded, and GCE.Ready() is called
+	var isReady = false;
+	this.isReady = function() { return isReady; };
 
 	// holds all the instances of entities
 	var Entities = {};
+	// order for entity component updates. these are updated after all entities are updated
 	var zOrder = [];
 	// holds blueprint for each entity
 	var EntityBlueprints = {};
+	// blueprints for components
 	var Components = {};
-
-	var lastUpdate;
 
 	this.Ready = function() {};
 
+	// time of last update, used for deltatime
+	var lastUpdate;
 	var gameLoop = function() {
 		var now = performance.now();
 		this.dt = (now - lastUpdate) / 1000;
 		lastUpdate = now;
 
+		// fill canvas with background colour
 		GCE.ctx.fillStyle = 'black';
 		GCE.ctx.fillRect(0, 0, GCE.canvas.width, GCE.canvas.height);
 
+		// update all entities
 		UpdateEntities();
 
+		// do loop again
 		requestAnimationFrame(gameLoop);
 	}
 
@@ -34,17 +42,22 @@ var GCE = new function() {
     	return _p8() + _p8(true) + _p8(true) + _p8();
 	}
 
+	// this is the last function to be called for ready state, it actually starts the loop adter calling GCE.Ready()
 	this.init = function() {
+		isReady = true;
 		this.Ready();
 		lastUpdate = performance.now();
 		requestAnimationFrame(gameLoop);
 	}
 	
+	// used to setup GCE sprites, entities, components, images, parameters, etc.
 	this.Start = function(parameters) {
+		// required parameters
 		var defaultParameters = {
 			sprites: [],
 			entities: []
 		};
+		// get default parameters if something is missing
 		parameters = SetDefaultParameters(parameters, defaultParameters);
 
 		// ensure that a canvas id was passed
@@ -53,6 +66,7 @@ var GCE = new function() {
 			return false; //if no canvas was passed exit early
 		}
 
+		// ensure the passed ID exists
 		var canvas = document.getElementById(parameters.canvas);
 		if(canvas == undefined) {
 			console.error('Invalid Canvas ID!');
@@ -63,81 +77,112 @@ var GCE = new function() {
 			this.ctx = canvas.getContext('2d');
 			this.ctx.fillStyle = 'black';
 			this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+			// draw loading bar here?
 		}
 
+		// add passed sprites to loadqueue
 		this.Loader.LoadSprites(parameters.sprites);
+		// add passed entities to loadqueue
 		this.Loader.LoadEntities(parameters.entities);
-
-		// lastUpdate = performance.now();
-		// requestAnimationFrame(gameLoop);
 	}
 
 	this.CreateBlueprint = function(blueprintName, blueprint) {
-		EntityBlueprints[blueprintName] = blueprint
+		EntityBlueprints[blueprintName] = blueprint;
 	}
 
 	this.CreateEntity = function(entityName, properties) {
+		// ensure the blueprint exists
 		if(EntityBlueprints.hasOwnProperty(entityName)) {
+			// assign created instance to a temporary variable
 			var newEntity = new EntityBlueprints[entityName];
+			// assign its GUID and ENTITY_TYPE
 			newEntity.GUID = this.GenerateGUID();
 			newEntity.ENTITY_TYPE = entityName;
+			
+			// add created entity to Entities object
 			Entities[newEntity.GUID] = newEntity;
+			// add created entities GUID to zOrder array
 			zOrder.push(newEntity.GUID);
+			// call the entities create function if it has one, passing the properties
 			if(typeof newEntity.Create === 'function') { newEntity.Create(properties); }
 
 			// add required components
 			newEntity.Components = {};
+			
+			// used to add new components to an entity
 			newEntity.AddComponent = function(componentName, componentType, properties) {
+				// get a new entity of the required type, passing the properties for the Component.Create() function
 				var newComponent = GCE.GetComponent(componentType, properties);
+				// if a new component was created, add it to the entity
 				if(newComponent) {
 					newEntity.Components[componentName] = newComponent;
 					return true;
 				}
 				return false;
 			}
+
+			// used to get a existing component on an entity
 			newEntity.GetComponent = function(componentName) {
 				if(this.Components.hasOwnProperty(componentName)) return this.Components[componentName];
 				return false;
 			}
+
+			// add any required components that are stated int he blueprint
 			if(newEntity.hasOwnProperty('requiredComponents')) {
 				for(var i in newEntity.requiredComponents) {
 					var currentComponent = newEntity.requiredComponents[i];
 					
+					// get component properties from the entity properties if they exist
 					var componentProperties = {};
 					if(properties.hasOwnProperty(currentComponent)) { componentProperties = properties[currentComponent]; }
 					
+					// get the new component passing the properties and entity GUID for Component.Owner
 					var newComponent = this.GetComponent(currentComponent, componentProperties, newEntity.GUID);
+					// add the component if it was created successfully
 					if(newComponent) { newEntity.Components[currentComponent] = newComponent; }
 				}
 			}
+			// return the entities GUID
 			return newEntity.GUID;
 		}
 		return false;
 	}
 
+	// used to create a new blueprint for a component
 	this.NewComponent = function(componentName, component, cantOverwrite) {
+		// ensure that it does not overwrite any components with the cantOverwrite property
 		if(Components.hasOwnProperty(componentName)) {
 			if(Components[componentName].hasOwnProperty('cantOverwrite')) {
 				console.error('Cant overwrite component \'' + componentName + '\'')
 				return false;
 			}
 		}
+		// set the cantOverwrite property if necessary
 		if(cantOverwrite) component.cantOverwrite = true;
+		// add the new component to Components object
 		Components[componentName] = component;
 	}
 
+	// used to create and return a new component
 	this.GetComponent = function(componentName, properties, GUID) {
+		// ensure the component type exists
 		if(Components.hasOwnProperty(componentName)) {
+			// create the new component
 			var newComponent = new Components[componentName];
+			// if a GUID was passed, assign that entity to newComponent.Owner
 			if(GUID) { newComponent.Owner = this.GetEntityByID(GUID); }
+			// ensure a Create and Update function exists
 			if(!newComponent.hasOwnProperty('Create')) { newComponent.Create = function() {} }
 			if(!newComponent.hasOwnProperty('Update')) { newComponent.Update = function() {} }
+			// call newComponent.Create() passing in the component properties
 			newComponent.Create(properties);
+			// return the created component
 			return newComponent;
 		}
 		return false;
 	}
 
+	// used to ensure that 
 	function SetDefaultParameters(parameters, defaultParameters) {
 		for(var p in parameters) {
 			var currentParameter = parameters[p];
@@ -181,6 +226,7 @@ var GCE = new function() {
 		var scriptsLoaded = 0;
 		var spritesToLoad, entitiesToLoad;
 
+		// adds a script tag to head for required libraries
 		var requiredScripts = ['http://code.jquery.com/jquery-1.11.3.min.js', 'http://requirejs.org/docs/release/2.1.20/minified/require.js']
 		for(var i in requiredScripts) {
 			var newScript = document.createElement('script');
@@ -192,6 +238,7 @@ var GCE = new function() {
 			document.getElementsByTagName('head')[0].appendChild(newScript);
 		}
 
+		// initialize GCE if the last object has been loaded. note: this does not include scripts
 		this.LoadObject = function() {
 			loaded++;
 			if(loaded >= toLoad) GCE.init();
@@ -202,6 +249,7 @@ var GCE = new function() {
 			for(var i in spritesToLoad) {
 				var currentSprite = spritesToLoad[i];
 				toLoad++;
+				// grab the sprite json from sprites/ folder and load it in
 				$.getJSON('sprites/' + currentSprite + '.json', function(data) {
 					console.log(data);
 					var sprite = data.image.split('.')[0];
@@ -219,6 +267,7 @@ var GCE = new function() {
 		}
 
 		this.LoadImage = function(name, file) {
+			// dont load the image if it has already been loaded, this allows multiple sprites to use the same image
 			if(!LoadedImages.hasOwnProperty(name)) {
 				toLoad++;
 				var tempImage = new Image();
@@ -231,6 +280,7 @@ var GCE = new function() {
 		};
 
 		this.LoadSprites = function(sprites) {
+			// assign the sprites array to spritesToLoad for loading later (after scripts)
 			spritesToLoad = sprites;
 		};
 
@@ -261,6 +311,7 @@ var GCE = new function() {
 		return false;
 	}
 
+	// REMOVE LATER these are for debug purposes
 	this.GetEntities = function(entityType, callback) {
 		return Entities;
 	}
@@ -270,7 +321,16 @@ var GCE = new function() {
 	}
 }
 
+/*
+~~~~~~~~~~~~~~~~~~~~
+|  				   |
+|	 COMPONENTS    |
+|				   |
+~~~~~~~~~~~~~~~~~~~~
+*/
+
 GCE.NewComponent('Transform', function() {
+	// default values
 	this.Position = {
 		x: 0,
 		y: 0
@@ -280,6 +340,7 @@ GCE.NewComponent('Transform', function() {
 		y: 0
 	}
 	this.Create = function(properties) {
+		// dont do anything if no properties were passed
 		if(properties == undefined) return false;
 		if(properties.hasOwnProperty('Position')) {
 			this.Position.x = properties.Position.x;
@@ -305,10 +366,13 @@ GCE.NewComponent('SpriteRenderer', function() {
 	}
 
 	this.Update = function() {
+		// get the entities Transform component
 		var Transform = this.Owner.GetComponent('Transform');
+		// figure out where to draw it based on Transform.Position and Transform.Anchor
 		var drawX = Transform.Position.x + Transform.Anchor.x * -1;
 		var drawY = Transform.Position.y + Transform.Anchor.y * -1;
 		var frame = this.GetFrame();
+		// draw the image
 		GCE.ctx.drawImage(this.img, frame.x, frame.y, frame.width, frame.height, drawX, drawY, frame.width, frame.height);
 	}
 
