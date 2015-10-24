c95d1b4a596f8a4906c6bc456b588c7653a8d925
GuineaGuy99 <GuineaGuy99.Game@gmail.com>
2015-10-25 01:27:12 +1100

    Fixed viewport stuff and added controls!

--- core/Viewport.js
+++ core/Viewport.js
@@ -1,14 +1,14 @@
 Nova.Viewport = new function(){
 	this.Position = new Nova.System.Vector2();
 	this.Size = new Nova.System.Vector2();
-	this.Scale = new Nova.System.Vector2();
-	this.rotation = 0;
+	this.Scale = new Nova.System.Vector2(1, 1);
+	this.Rotation = 0;
 
 	this.Apply = function(){
-		Nova.ctx.translate(-this.Position.X, -this.Position.Y);
-		Nova.ctx.translate(this.Position.X + (this.Size.X/2), this.Position.Y + (this.Size.Y/2));
-		Nova.ctx.rotate(-this.rotation * Math.PI/180);
-		Nova.ctx.scale(this.Scale.X, this.Scale.Y);
-		Nova.ctx.translate(-(this.Position.X + (this.Size.X/2)), -(this.Position.Y + (this.Size.Y/2)));
+		Nova.ctx.translate(-Nova.Viewport.Position.X, -Nova.Viewport.Position.Y);
+		Nova.ctx.translate(Nova.Viewport.Position.X + (Nova.Viewport.Size.X/2), Nova.Viewport.Position.Y + (Nova.Viewport.Size.Y/2));
+		Nova.ctx.rotate(-Nova.Viewport.Rotation * Math.PI/180);
+		Nova.ctx.scale(Nova.Viewport.Scale.X, Nova.Viewport.Scale.Y);
+		Nova.ctx.translate(-(Nova.Viewport.Position.X + (Nova.Viewport.Size.X/2)), -(Nova.Viewport.Position.Y + (Nova.Viewport.Size.Y/2)));
 	}
 }
--- entities/Topdown.js
+++ entities/Topdown.js
@@ -17,6 +17,27 @@ Nova.CreateBlueprint('Topdown', function() {
 		var mouseAngle = Nova.System.angleTowards(Position.x, Position.y, Nova.Input.Mouse.x, Nova.Input.Mouse.y);
 		Transform.SetAngle(Nova.System.angleLerp(Transform.GetAngle(), mouseAngle, 15 * Nova.dt));
 		// if(Nova.Input.Mouse.Pressed) Nova.Audio.Play('laser9');
+
+		//Move Viewport
+		if (Nova.Input.KeyDown('I')) Nova.Viewport.Position.Y -= 100 * Nova.dt;
+		if (Nova.Input.KeyDown('J')) Nova.Viewport.Position.X -= 100 * Nova.dt;
+		if (Nova.Input.KeyDown('K')) Nova.Viewport.Position.Y += 100 * Nova.dt;
+		if (Nova.Input.KeyDown('L')) Nova.Viewport.Position.X += 100 * Nova.dt;
+
+		//Rotate Viewport
+		if (Nova.Input.KeyDown('U')) Nova.Viewport.Rotation += 100 * Nova.dt;
+		if (Nova.Input.KeyDown('O')) Nova.Viewport.Rotation -= 100 * Nova.dt;
+
+		//Zoom Viewport
+		if (Nova.Input.KeyDown('OPENBRACKET')) {
+			Nova.Viewport.Scale.X -= 1 * Nova.dt
+			Nova.Viewport.Scale.Y -= 1 * Nova.dt
+		}
+		if (Nova.Input.KeyDown('CLOSEBRACKET')){
+			Nova.Viewport.Scale.X += 1 * Nova.dt
+			Nova.Viewport.Scale.Y += 1 * Nova.dt
+		}
+
 	}
 
 	this.SetTarget = function(x, y) {
