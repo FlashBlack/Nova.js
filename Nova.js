1e037471f0c90002a423cdc8826eac1eb97dcaeb
Josh Boomhower <encryptedcow@gmail.com>
2015-10-24 00:49:19 -0600

    renamed

--- Nova.js
+++ Nova.js
@@ -6,59 +6,6 @@ var Nova = new function() {
 	var started = false;
 	this.isReady = function() { return isReady; };
 
-	// general functions and values
-	this.System = new function() {
-		this.GenerateGUID = function() {
-			function _p8(s) {
-	        	var p = (Math.random().toString(16)+"000000000").substr(2,8);
-	        	return s ? "-" + p.substr(0,4) + "-" + p.substr(4,4) : p ;
-	    	}
-	    	return _p8() + _p8(true) + _p8(true) + _p8();
-		}
-		this.angleTowards = function(x1, y1, x2, y2) {
-			var dx = x2 - x1;
-			var dy = y2 - y1;
-			return this.toDegrees(Math.atan2(dy, dx));
-		}
-		this.distance = function(x1, y1, x2, y2) {
-			var dx = x2 - x1;
-			var dy = y2 - y1;
-			return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
-		}
-		this.rotateAround = function(pointX, pointY, originX, originY, angle) {
-			angle = this.toRadians(angle) * -1;
-			pointX++;
-			pointY++;
-			originX++;
-			originY++;
-		    return {
-		        x: (Math.cos(angle) * (pointX-originX) - Math.sin(angle) * (pointY-originY) + originX) - 1,
-		        y: (Math.sin(angle) * (pointX-originX) + Math.cos(angle) * (pointY-originY) + originY) - 1
-		    };
-		}
-		this.lerp = function(a, b, x) {
-			return a + x * (b - a);
-		}
-		this.angleLerp = function(a, b, x) {
-			return a + ((((((b - a) % 360) + 540) % 360) - 180) * x);
-		}
-		this.clamp = function(x, a, b) {
-			return Math.min(Math.max(x, a), b);
-		}
-		this.midpoint = function(x1, y1, x2, y2) {
-			return {
-				x: (x1 + x2) / 2,
-				y: (y1 + y2) / 2
-			}
-		}
-		this.toRadians = function(angle) {
-			return angle * (Math.PI / 180);
-		}
-		this.toDegrees = function(angle) {
-			return angle * (180 / Math.PI);
-		}
-	}
-
 	// holds all the instances of entities
 	var Entities = {};
 	// order for entity component updates. these are updated after all entities are updated
@@ -517,10 +464,6 @@ var Nova = new function() {
 				collider1.bboxbottom > collider2.bboxtop);
 	}
 
-	this.Debug = new function() {
-		// var 
-	}
-
 	this.GetEntityByID = function(ID) {
 		if(Entities.hasOwnProperty(ID)) return Entities[ID];
 		return false;
@@ -533,5 +476,73 @@ var Nova = new function() {
 
 	this.GetBlueprints = function() {
 		return EntityBlueprints;
+	}
+}
+
+Nova.System = new function() {
+	this.GenerateGUID = function() {
+		function _p8(s) {
+        	var p = (Math.random().toString(16)+"000000000").substr(2,8);
+        	return s ? "-" + p.substr(0,4) + "-" + p.substr(4,4) : p ;
+    	}
+    	return _p8() + _p8(true) + _p8(true) + _p8();
+	}
+	this.angleTowards = function(x1, y1, x2, y2) {
+		var dx = x2 - x1;
+		var dy = y2 - y1;
+		return this.toDegrees(Math.atan2(dy, dx));
+	}
+	this.distance = function(x1, y1, x2, y2) {
+		var dx = x2 - x1;
+		var dy = y2 - y1;
+		return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+	}
+	this.rotateAround = function(pointX, pointY, originX, originY, angle) {
+		angle = this.toRadians(angle) * -1;
+		pointX++;
+		pointY++;
+		originX++;
+		originY++;
+	    return {
+	        x: (Math.cos(angle) * (pointX-originX) - Math.sin(angle) * (pointY-originY) + originX) - 1,
+	        y: (Math.sin(angle) * (pointX-originX) + Math.cos(angle) * (pointY-originY) + originY) - 1
+	    };
+	}
+	this.lerp = function(a, b, x) {
+		return a + x * (b - a);
+	}
+	this.angleLerp = function(a, b, x) {
+		return a + ((((((b - a) % 360) + 540) % 360) - 180) * x);
+	}
+	this.clamp = function(x, a, b) {
+		return Math.min(Math.max(x, a), b);
+	}
+	this.midpoint = function(x1, y1, x2, y2) {
+		return {
+			x: (x1 + x2) / 2,
+			y: (y1 + y2) / 2
+		}
+	}
+	this.toRadians = function(angle) {
+		return angle * (Math.PI / 180);
+	}
+	this.toDegrees = function(angle) {
+		return angle * (180 / Math.PI);
+	}
+}
+
+Nova.Debug = new function() {
+	var drawColliders = true;
+	
+	this.toggleColliders = function(state) {
+		if(state == undefined) drawColliders = !drawColliders;
+		else drawColliders = state;
+	}
+
+	this.drawColliders = function() {
+		if(!drawColliders) return;
+		var solids = Nova.getSolids();
+		console.log(solids);
+		// for()
 	}
 }
