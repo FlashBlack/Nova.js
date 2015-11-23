bc8783e31c0c15713393b2ab0b9cdb585e76758f
Josh Boomhower <encryptedcow18@gmail.com>
2015-11-23 13:27:17 +0000

    much progresso

--- engine/game.js
+++ engine/game.js
@@ -1,1 +1,9 @@
+var test;
+
+Nova.Ready = function() {
+    require(['Nova/Entities'], function(Entities) {
+        test = Entities.createEntity('testEntity', {})
+    })
+}
+
 Nova.Start('Project.json');
